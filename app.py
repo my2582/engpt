e079a69b32de1f90cbbad6af4acca54d79cc3ab4
Minsu Yeom <my2582@columbia.edu>
2023-03-12 23:25:52 +0900

    added some functions, still writing.

---
+++ app.py
@@ -0,0 +1,130 @@
+import os
+import streamlit as st
+import pandas as pd
+from pandas import DataFrame
+from google.oauth2 import service_account
+import gspread
+import openai
+import numpy as np
+from transformers import GPT2TokenizerFast
+from sentence_transformers import SentenceTransformer
+import datetime
+from streamlit_chat import message
+
+
+def check_password() -> bool:
+    """
+    Returns `True` if the user had a correct password.
+    :return:
+    """
+
+    def password_entered() -> bool:
+        """
+        Checks whether a password entered by the user is correct.
+        """
+        if st.session_state['username'] in st.secrets['password'] and \
+                st.session_state['password'] == st.secrets['passwords'][st.session_state['username']]:
+            st.session_state['password_correct'] = True
+            del st.session_state['password']  # Never store password
+        else:
+            st.session_state['password_state'] = False
+
+    if 'password_correct' not in st.session_state:
+        # First run, show inputs for username + password.
+        st.text_input('아이디(ID)', on_change=password_entered, key='username')
+        st.text_input('비밀번호', type='password', on_change=password_entered, key='password')
+        return False
+    elif not st.session_state['password_correct']:
+        st.text_input('아이디(ID)', on_change=password_entered, key='username')
+        st.text_input('비밀번호', type='password', on_change=password_entered, key='password')
+        st.error('알 수 없는 아이디(ID) 또는 비밀번호 오류입니다.')
+        return False
+    else:
+        return True
+
+
+if check_password():
+    if 'kept_username' not in st.session_state:
+        st.session_state['kept_username'] = st.session_state['username']
+
+        st.set_page_config(page_title = "서툰 영어로 말해도 문법교정은 물론 자연스러운 표현도 알려 줘요.")
+        st.title('EngChat with ChatGPT: 영어, 이제 ChatGPT에게 배우세요.')
+        st.subheader('24/7 지치지 않는 원어민을 준비했어요.')
+        st.markdown('EngChat with ChatGPT는 openai가 공개한 ChatGPT 3.5 turbo API를 사용하여 한국인 영어 교육을 위해 개발되었어요.')
+
+    @st.cache_resource
+    def load_tokenizer():
+        return GPT2TokenizerFast.from_pretrained('gpt2')
+
+    @st.cache_resource
+    def access_sheet(sheet_name: str):
+        '''
+        Access the Google's sheets.
+        :param sheet_name:
+        :return:
+        '''
+
+        scope = ['https://spreadsheets.google.com/feeds',
+                 'https://www.googleapis.com/auth/drive']
+        credentials = service_account.Credentials.from_service_account_info(st.secrets['gcp_service_account'],
+                                                                            scope=scope)
+
+        gc = gspread.authorize(credentials)
+        # open by a title that appears on a Google Sheet.
+        sheet = gc.open('engpt-db').worksheet(sheet_name)
+
+        return sheet
+
+    def get_max_num_tokens() -> int:
+        '''
+        The maximum number of tokens a pre-trained NLP model can take.
+        :return:
+        '''
+
+        return 2046
+
+    def construct_prompt(query: str,  method: str):
+        '''
+        Construct the prompt to answer the query. The prompt is composed of the query from the user.
+        :param query: str
+            Query.
+        :param method:
+            indicates which model to use.
+        :return:
+        '''
+
+        MAX_SECTION_LEN =  get_max_num_tokens()
+        SEPARATOR = '\n* '
+        tokenizer = load_tokenizer()
+        separator_len = len(tokenizer.tokenize(SEPARATOR))
+        chosen_sections = []
+        chosen_sections_len = 0
+
+        header = '''
+You are an ESL teacher answering students' questions. Replay as an ESL teacher.
+
+Example conversion:
+        
+Student: Hi!
+        
+Teacher: Hi there! How can I help you?
+        
+Student: I want to learn English from you. Would you help me?
+        
+Teacher: Hi! I would be happy to help you with your English language learning. What kind of help do you need?'''
+
+        prompt = '''
+Student: For the following text in double quotations, do: answer, change it to standard English,
+point out every grammar mistake, paraphrase to make it sound more natural to English native speakers,
+and finally respond everything in two different languages, one by one, English and Korean.'''
+
+        prompt = f'{header}\n\n{prompt}\n'
+
+        return prompt
+
+    def record_question_answer(user, query, answer):
+        '''
+        record the query, prompt and answer in the database (google sheet).
+        '''
+
+        sheet = access_sheet('Q&A')
