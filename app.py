8c9396a71bc259d8b882c17a9441bf047796b694
Minsu Yeom <my2582@columbia.edu>
2023-03-20 22:17:39 +0900

    columns([1, 5, 1])

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ st.title('영어, 이제 ChatGPT에게 배우세요.')
 st.subheader('지치지 않는 원어민 AI를 준비했어요. 문법적 오류와 원어민 표현을 함께 배워 보세요.')
 
 image = Image.open('./images/example_chat.png')
-col1, col2, col3 = st.columns([0.2, 5, 0.2])
+col1, col2, col3 = st.columns([1, 5, 1])
 col2.image(image, caption='대화 예시', use_column_width='auto')
 
 method = 'openai'
